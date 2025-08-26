5d084c68b672795f474cae60ecf01f4d53179466
Ali Ansari <maliansari.coder@gmail.com>
2025-08-27 03:35:11 +0500

    resolved icons appearing black

--- app/components/Icons/Github.vue
+++ app/components/Icons/Github.vue
@@ -20,7 +20,7 @@
       <g
         id="Dribbble-Light-Preview"
         transform="translate(-140.000000, -7559.000000)"
-        fill="#000000"
+        fill="#ffffff"
       >
         <g id="icons" transform="translate(56.000000, 160.000000)">
           <path
